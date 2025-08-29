3ce971bf82e1c569448dda9c3acbdd7e6b83534f
wendellxavier25 <wendellx25@gmail.com>
2025-08-29 08:41:08 -0300

    modification in view pedido

--- pedido/views.py
+++ pedido/views.py
@@ -11,23 +11,25 @@ from django.contrib import messages
 from utils import utils
 from .models import Pedido, ItemPedido
 
-class DispatchLoginRequired(View):
+class DispatchLoginRequiredMixin(View):
     def dispatch(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('perfil:criar')
         
         return super().dispatch(*args, **kwargs)
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(usuario=self.request.user)
+        return qs
 
-class Pagar(DispatchLoginRequired, DetailView):
+class Pagar(DispatchLoginRequiredMixin, DetailView):
     template_name = 'pagar.html'
     model = Pedido
     pk_url_kwarg = 'pk'
     context_object_name = 'pedido'
 
-    def get_queryset(self, *args, **kwargs):
-        qs = super().get_queryset(*args, **kwargs)
-        qs = qs.filter(usuario=self.request.user)
-        return qs
+    
 
 
 class SalvarPedido(View):
@@ -74,7 +76,7 @@ class SalvarPedido(View):
 
             pedido = Pedido(usuario=self.request.user,
                             total=valor_total_carrinho, 
-                            qtd_total=valor_total_carrinho, 
+                            qtd_total=qtd_total_carrinho, 
                             status='C')
             
             pedido.save()
@@ -97,7 +99,12 @@ class SalvarPedido(View):
         return redirect(reverse('pedido:pagar', kwargs={'pk': pedido.pk}))
 
 class Detalhe(View):
-    pass
+    def get(self, *args, **kwargs):
+        return ...
 
-class Lista(View):
-    pass
+class Lista(DispatchLoginRequiredMixin, ListView):
+    model = Pedido
+    context_object_name = 'pedidos'
+    template_name = 'lista.html'
+    paginate_by = 10
+    ordering = ['-id']
